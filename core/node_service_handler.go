da48f3dec6a7b1bb47150fcaec91c1835b09dec5
Bjornskjald <github@bjorn.ml>
2019-07-14 18:57:15 +0200

    fix: Fixed executing Node services

--- core/node_service_handler.go
+++ core/node_service_handler.go
@@ -1,6 +1,9 @@
 package core
 
-import "os/exec"
+import (
+	"os"
+	"os/exec"
+)
 
 type NodeServiceHandler struct {
 	App              *App
@@ -19,7 +22,11 @@ func (nsh *NodeServiceHandler) LoadService(instanceID string) {
 	service := nsh.App.sm.FindServiceWithName(instance.ModuleName)
 
 	// Startup service
-	nsh.ServiceProcesses[instanceID] = exec.Command("node", "services/"+service.Name+"/"+service.EntryPoint, instance.ID)
+	command := exec.Command("node", "services/"+service.Name+"/"+service.EntryPoint, instance.ID)
+	nsh.ServiceProcesses[instanceID] = command
+	command.Stdout = os.Stdout
+	command.Stdin = os.Stdin
+	command.Run()
 }
 
 func (nsh *NodeServiceHandler) ShutdownService(instanceID string) {
